2b2684182c66b82437b31bb458435dd673e8bb7d
resist15 <sourav@aospa.co>
2024-07-23 10:18:44 +0530

    SocialPulse: Create basic CRUD endpoints
    Change-Id: I31aa0573310fb23d7900756ad4a105b4d0f434c4

---
+++ main.py
@@ -0,0 +1,66 @@
+from typing import Optional
+from fastapi import FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1","id": 1},{"title": "favorite foods","content": "i like pizza", "id": 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+@app.get('/posts')
+def get_post():
+    return {"data": my_posts}
+
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+def create_post(new_post: Post):
+    my_posts.append(new_post.model_dump())
+    return {"title is": f"{new_post.title}",
+            "content is": f"{new_post.content}"}
+
+@app.get("/posts/{id}")
+def get_post_single(id: int):
+
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id {id} not found")
+    return {"post_detail": post}
+
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Post with id {id} not found')
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
+    post_dict = post.model_dump()
+    print(type(post_dict))
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"message": post_dict}
+
