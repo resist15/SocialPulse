95a18fce8f857d60ae8bae6718434d4b58350356
resist15 <sourav@aospa.co>
2024-07-28 12:54:04 +0530

    SocialPulse: Fix Status codes for auth endpoint
    Change-Id: I08b630a18405b2538ec64a6a5103621cc5312db9

--- app/auth.py
+++ app/auth.py
@@ -12,10 +12,10 @@ def login(creds: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(da
     user = db.query(models.User).filter(models.User.email == creds.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalied Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalied Credentials")
 
     if not utils.verify(creds.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Invalid Credentials")
  
     access_token = oauth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
