502d52ccd52eb73bc344c3a684394252c2290553
resist15 <sourav@aospa.co>
2024-08-05 14:48:44 +0530

    SocialPulse: Setup CORS middleware
    enable all origins for now, will be narrrowed down in future
    
    Change-Id: I4527f3c589e4a88806fcef54f392f71a6fdda0d9

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-from fastapi import  FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import user, post, auth, vote
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
