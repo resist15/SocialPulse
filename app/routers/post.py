2139d8a9b989c9484d51612e19a28a77414db073
resist15 <sourav@aospa.co>
2024-08-12 11:15:05 +0530

    SocialPulse: Setup testing database and import tests for all endpoints
    fix get single post endpoint
    
    Change-Id: I495176413e143caa2803a21e406c42e247c6c3f6

--- app/routers/post.py
+++ app/routers/post.py
@@ -32,7 +32,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),current_
 @router.get("/{id}",response_model=schemas.PostOut)
 def get_post_single(id: int, db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user)):
 
-    post =  db.query(models.Post, func.count(models.Vote.post_id).label("likes")).join(models.Vote,models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).first()
+    post =  db.query(models.Post, func.count(models.Vote.post_id).label("likes")).join(models.Vote,models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id {id} not found")
     return post
